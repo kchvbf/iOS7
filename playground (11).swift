8c5fa3e41abcf6721ec7f541603a32c39aa892c4
kchvbf <109752188+kchvbf@users.noreply.github.com>
2024-02-20 22:53:25 +0300

    Add files via upload

---
+++ playground (11).swift
@@ -0,0 +1,14 @@
+protocol Hotel {
+    var roomCount: Int { get }
+    
+    init(roomCount: Int)
+}
+
+class HotelAlfa: Hotel {
+    var roomCount: Int
+    
+    required init(roomCount: Int) {
+        self.roomCount = roomCount
+    }
+}
+
